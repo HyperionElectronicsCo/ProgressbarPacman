f42b206e09dfec8f8a108ec271ca57e48d40b966
HyperionElectronicsCo <HyperionElectronicsCo@hotmail.com>
2021-08-20 01:17:45 +0100

    Full

--- app/src/main/java/com/example/progressbar/MainActivity.java
+++ app/src/main/java/com/example/progressbar/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     Handler handler = new Handler();
 	private int progressStatus = 0;
     private TextView textView;
-	
-	
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 		final Drawable thumb2 = getResources().getDrawable(R.drawable.Pacman2);
 		Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-		
+
         progressBar = (SeekBar) findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
 		textView = (TextView) findViewById(R.id.textView);
@@ -33,64 +33,57 @@ public class MainActivity extends AppCompatActivity {
 		progressBar.setBackgroundResource(R.drawable.n2az1ncp5auo2czwv055_20210819060842583);
 		progressBar.setMin(0);
 		progressBar.setMax(100);
-	// Start long running operation in a background thread
-	new
-	Thread(
-	new
-	Runnable() {
-	public void run()
-	{
-			progressBar.setVisibility(View.VISIBLE);
-		
-		while
-		(progressStatus <
-		 100
-		 ) { progressStatus +=
-				1
-				;
+		// Start long running operation in a background thread
+		new
+			Thread(
+			new
+			Runnable() {
+				public void run() {
+					progressBar.setVisibility(View.VISIBLE);
+
+					while
+					(progressStatus <
+					 100
+					 ) { progressStatus +=
+							1
+							;
 // Update the progress bar and display the
 //current value in the text view
-			handler.post(
-				new
-				Runnable() {
-					public void run()
-					{
-						progressBar.setProgress(progressStatus); textView.setText(progressStatus+
-																				"/"
-																				+progressBar.getMax()); 
-						if
-						((progressBar.getProgress() == 100))
-						{
-							progressBar.setVisibility(View.GONE);
-							textView.setVisibility(View.GONE);
-						}
-						if ((progressStatus % 2) == 0) {
-							// number is even
-							progressBar.setThumb(thumb2);
-						}
+						handler.post(
+							new
+							Runnable() {
+								public void run() {
+									progressBar.setProgress(progressStatus); textView.setText(progressStatus +
+																							  "/"
+																							  + progressBar.getMax()); 
+									if
+									((progressBar.getProgress() == 100)) {
+										progressBar.setVisibility(View.GONE);
+										textView.setVisibility(View.GONE);
+									}
+									if ((progressStatus % 2) == 0) {
+										// number is even
+										progressBar.setThumb(thumb2);
+									} else {
+										// number is odd
+										progressBar.setThumb(thumb);
+									}
+								} 
+							});
 
-						else {
-							// number is odd
-							progressBar.setThumb(thumb);
-						}
-																				} 
-																			});
-																				
-			try
-			{
+						try {
 // Sleep for 200 milliseconds.
-				Thread.sleep(
-					69
-				);
-				}
-			catch
-			(InterruptedException e) { e.printStackTrace(); 
-			} 
-		}
-	} 
+							Thread.sleep(
+								69
+							);
+						} catch
+						(InterruptedException e) { e.printStackTrace(); 
+						} 
+					}
+				} 
 			}).start();
-		
-		
-}
+
+
+	}
 
 }
